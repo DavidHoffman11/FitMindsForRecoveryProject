8d5196f4b5f88e9ddf163f79e4d8d66ea6cb07fe
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2024-10-07 20:21:33 -0500

    10/7 changes

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -1,9 +1,16 @@
+import './Contact.css'
+import React from 'react';
+import { Card } from 'primereact/card';
+
 function Contact () {
     return (
-      <div>
-        <h1>Contact Page</h1>
-        <p>Welcome to the contact page!</p>
-      </div>
+      <div className="contact-container">
+      <Card className="contact-card">
+        <h2>Contact Us</h2>
+        <p className="contact-text">If you have any questions or need further assistance, feel free to reach out!</p>
+        <p className="contact-info">Email: <a href="mailto:fitmindsforrecovery@gmail.com">fitmindsforrecovery@gmail.com</a></p>
+      </Card>
+    </div>
     );
   };
   
