212ff311ccf29cf103ccab515991ea97d7362c8d
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2024-10-08 20:04:00 -0500

    finish basic page template

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -1,12 +1,18 @@
+import './About.css';
 import React from 'react';
+import { Card } from 'primereact/card';
 
-function About () {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <p>Welcome to the about page!</p>
-    </div>
-  );
+function About() {
+    return (
+        <div className="about-container">
+            <Card className="about-card">
+                <h1 className="about-title">About Us</h1>
+                <p className="about-mission">
+                    At Fit Minds for Recovery, we are dedicated to helping individuals on their journey to recovery by providing fitness programs, mentorship, and a strong community.
+                </p>
+            </Card>
+        </div>
+    );
 };
 
 export { About };
--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -5,11 +5,15 @@ import { Card } from 'primereact/card';
 function Contact () {
     return (
       <div className="contact-container">
-      <Card className="contact-card">
-        <h2>Contact Us</h2>
-        <p className="contact-text">If you have any questions or need further assistance, feel free to reach out!</p>
-        <p className="contact-info">Email: <a href="mailto:fitmindsforrecovery@gmail.com">fitmindsforrecovery@gmail.com</a></p>
-      </Card>
+        <Card className="contact-card">
+          <h1 className="contact-title">Contact Us!</h1>
+          <p className="contact-text">
+            If you have any questions or need further assistance, feel free to reach out!
+          </p>
+          <p className="contact-info">
+            Email: <a href="mailto:fitmindsforrecovery@gmail.com">fitmindsforrecovery@gmail.com</a>
+          </p>
+        </Card>
     </div>
     );
   };
