753207e6c0d3948209adbdac87d19b9c4f160f76
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2024-10-08 20:29:08 -0500

    Update About.tsx

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -5,12 +5,12 @@ import { Card } from 'primereact/card';
 function About() {
     return (
         <div className="about-container">
-            <Card className="about-card">
-                <h1 className="about-title">About Us</h1>
-                <p className="about-mission">
-                    At Fit Minds for Recovery, we are dedicated to helping individuals on their journey to recovery by providing fitness programs, mentorship, and a strong community.
-                </p>
-            </Card>
+          <Card className="about-card">
+              <h1 className="about-title">About Us</h1>
+              <p className="about-mission">
+                  At Fit Minds for Recovery, we are dedicated to helping individuals on their journey to recovery by providing fitness programs, mentorship, and a strong community.
+              </p>
+          </Card>
         </div>
     );
 };
