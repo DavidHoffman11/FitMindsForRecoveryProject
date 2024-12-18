7cbaf557c9dee6cd205a698c68e5d71bc2941119
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2024-12-17 19:51:30 -0600

    mission

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -2,8 +2,11 @@ import './About.css';
 import React from 'react';
 
 function About() {
-    const missionStatement = `At Fit Minds for Recovery, we are dedicated to helping individuals on their journey 
-                              to recovery by providing fitness programs, mentorship, and a strong community.`
+    const missionStatement = `At Fit Minds for Recovery, we believe that a healthier lifestyle and the power of fitness can play a pivotal
+                              role in the recovery journey. Through the supportive community we're building and the outreach efforts empowered
+                              by our wonderful partners, we are dedicated to helping individuals reclaim their lives from addiction. We seek to
+                              provide the resources, education, and encouragement needed for long-term healing and wellness. Together, we strive to 
+                              break the cycle of addiction and inspire lasting change in the lives of those we serve.`
 
     return (
         <div className="about-page">
