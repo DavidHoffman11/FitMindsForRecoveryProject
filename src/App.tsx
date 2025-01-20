a64cd193cb72495122d82cdfacbaa59caf7d0f6b
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2025-01-20 17:56:22 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ import { createBrowserRouter } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <About />,
   },
   {
     path: '/about',
-    element: <About />,
+    element: <Home />,
   },
   {
     path: '/events',
