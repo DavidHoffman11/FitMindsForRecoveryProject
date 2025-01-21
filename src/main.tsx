0aa1440b5c9054577316d9e73b36a2839aa63030
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2025-01-20 22:57:35 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import App from "./App";
-import React from "react";
 import "primeflex/primeflex.css"
 import "primeicons/primeicons.css"
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
