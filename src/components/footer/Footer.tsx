0de0c581c201d0734cfc8d3c87fd35c9e71ba975
David Hoffman <48999196+DavidHoffman11@users.noreply.github.com>
2025-01-30 16:42:08 -0600

    Footer Responsiveness

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import './Footer.css';
-import { Dialog } from 'primereact/dialog';
-import { useState } from 'react';
 import { FaInstagram } from 'react-icons/fa';
 
 export const Footer = () => {
-  const [isModalVisible, setModalVisible] = useState(false);
-
   return (
-    <div className="footer-container">
+    <footer className="footer-container">
       <div className="footer-content">
-        <p>Fit Minds for Recovery Project</p>
+        <p className="footer-text">Fit Minds for Recovery Project</p>
         <div className="social-icons">
-            <p>Connect with us!</p>
-            <a 
-                href="https://www.instagram.com/fitmindsforrecoveryproject/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-            >
-                <FaInstagram className="instagram-icon" />
-            </a>
+          <p className="footer-text">Connect with us!</p>
+          <a
+            href="https://www.instagram.com/fitmindsforrecoveryproject/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="instagram-icon" />
+          </a>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
